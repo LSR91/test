ab2bc37f49173fb25ac7bef59a4d9376c51ca446
Laura <62034907+LSR91@users.noreply.github.com>
2020-10-04 20:18:55 +0200

    Add files via upload

---
+++ posts/posts.js
@@ -0,0 +1,135 @@
+let pressPosts=[
+	{
+		id: 11,
+		titulo: "Nace la Asociación Amigos del Cine y Teatro de Castro Urdiales",
+		subtitulo: "Artículo publicado en la edición de papel de la separata dominical de El Diario Montañés el 15/03/2020",
+		texto: "",
+		firma: "",
+		fecha: "15 de marzo 2020",
+		pdf: true,
+		documento: 'doc11',
+		ruta: 'nace-asociacion-amigos-cine-teatro-castro-urdiales'
+	},
+
+	{
+		id: 10,
+		titulo: "Sin cine ni teatro en Castro Urdiales",
+		subtitulo: "Carta publicada en El Diario Montañés el 03/03/2020",
+		texto: "Es una verdadera pena que el actual consistorio de Castro Urdiales, dirigido por el PSOE y Ciudadanos, haya decidido lanzarse al vacío con un proyecto ambicioso, pero sin ninguna base firme, en lugar de seguir adelante con un proyecto que fue aprobado en la legislatura anterior y estaba ya listo para construir. Así, treinta años después de que el cine club Ágora fuera derribado (por un gobierno municipal del PSOE), los ciudadanos del municipio, el tercero de Cantabria en población, seguimos esperando a tener un cine-teatro. Vista la evolución del tema, todo indica que vamos a tener que seguir esperando bastante más. Qué rabia y tristeza ver que quitan algo que ya tenías al alcance de la mano porque, te dicen, te mereces algo mejor. Algo mejor, un proyecto ambicioso pero de lo cual no hay sino una idea, una intención. Entonces, ¿hasta cuándo tendremos que esperar? o mejor ¿por qué tenemos que esperar?",
+		firma: "Cinta Marraco",
+		fecha: "3 de marzo 2020",
+		pdf: false,
+		documento: '',
+		ruta: 'sin-cine-teatro-castro-urdiales'
+	},
+
+	{
+		id: 9,
+		titulo: "'No hay proyecto de Teatro en el Pachi Torre', dice la plataforma a favor de hacerlo en el Ágora",
+		subtitulo: "Artículo publicado en la edición de papel de la separata dominical de El Diario Montañés el 09/02/2020",
+		texto: "",
+		firma: "",
+		fecha: "9 de febrero 2020",
+		pdf: true,
+		documento: 'doc9',
+		ruta: 'no-proyecto-teatro-pachi-torre'
+	},
+
+	{
+		id: 8,
+		titulo: "Un grupo de castreños se constituirán en asociación para luchar por el Ágora",
+		subtitulo: "Artículo publicado en la edición de papel de la separata dominical de El Diario Montañés el 26/01/2020",
+		texto: "",
+		firma: "",
+		fecha: "26 de enero 2020",
+		pdf: true,
+		documento: 'doc8',
+		ruta: 'grupo-castreños-asociacion-agora'
+	},
+
+	{
+		id: 7,
+		titulo: "La Asociación Teatro en el Ágora presenta más de 2000 firmas en el Ayuntamiento",
+		subtitulo: "Artículo publicado en la edición de papel de la separata dominical de El Diario Montañés el 11/01/2020",
+		texto: "",
+		firma: "",
+		fecha: "11 de enero 2020",
+		pdf: true,
+		documento: 'doc7',
+		ruta: 'asociacion-teatro-agora-firmas-ayuntamiento'
+	},
+
+	{
+		id: 6,
+		titulo: "Teatro en Castro Urdiales",
+		subtitulo: "Carta publicada en El Diario Montañés el 26/12/2019",
+		texto: "La alcaldesa de Castro Urdiales, en recientes declaraciones a una emisora local, ha manifestado que solamente se va a construir el teatro para 800 localidades en el espacio próximo al polideportivo Pachi Torre y que su costo, según estudio efectuado por Gesvican, será de 'cuatro millones y pico de euros quedando fuera la excavación, que será un poco más'.Muy cerca de Castro Urdiales, en Algorta (Vizcaya) se acaba de construir un teatro de 800 localidades por un importe de 52 millones de euros, de los que 5,9 corresponden a la excavación, según se indica en el diario El Correo de fecha 24 de noviembre de 2017, así como en información publicada por el propio Ayuntamiento de Getxo. Naturalmente que sin conocer la propuesta de la alcaldesa se puede hacer una comparativa firme con el ya construido en Algorta, pero la diferencia de costo es escalofriante. Alguno de los organismos implicados en este tema debiera justificar debidamente y de forma urgente, con un proyecto técnico-económico adecuado, las cifras dadas por la alcaldesa y en caso de ser correctas aprovechar lo que parece una ocasión inmejorable para construir un teatro-cine en Castro Urdiales.",
+		firma: "Antonio Zornoza López",
+		fecha: "26 de diciembre 2019",
+		pdf: false,
+		documento: '',
+		ruta: 'teatro-en-castro-urdiales'
+	},
+
+	{
+		id: 5,
+		titulo: "Castro Urdiales necesita espacios culturales",
+		subtitulo: "Carta publicada en El Diario Montañés el 18/12/2019",
+		texto: "Pienso que Ataúlfo Argenta, Arturo Dúo y otras insignes personas derramarían lágrimas al ver que en su querido Castro no hay un espacio adecuado en donde poder disfrutar de una obra teatral, una película, un concierto... Y cuando ya parecía que iba a empezar el proyecto del espacio multiusos del Ágora, se rechaza argumentando que 'no es el proyecto que Castro merece'. Y me pregunto: ¿acaso se diseñó un proyecto sin considerar previamente las necesidades técnicas y de aforo?, ¿acaso se duda de la competencia de los técnicos de todas las disciplinas que colaboraron, lo desarrollaron y lo aprobaron? No quiero pensar que cuando dicen 'lo que Castro merece' se refieren a ese cartel que hasta no hace mucho, y siempre en campaña electoral, aparecía en los terrenos cercanos al polideportivo Pachi Torre, para una vez pasada ésta, desaparecer. Ni quiero pensar en la contrapartida a ese rechazo sea únicamente una idea. La ciudadanía de Castro Urdiales se merece que los responsables del rechazo nos muestren cuál es ese proyecto que sustituye al rechazado; en qué situación de desarrollo está; quién y cómo se va a financiar. Hasta el momento no se sabe nada.",
+		firma: "María del Carmen Aguado Tejero",
+		fecha: "18 de diciembre 2019",
+		pdf: false,
+		documento: '',
+		ruta: 'castro-urdiales-espacios-culturales'
+	},
+
+	{
+		id: 4,
+		titulo: "Un grupo de castreños apuesta por retomar el proyecto del Ágora",
+		subtitulo: "Artículo publicado en la edición de papel de la separata dominical de El Diario Montañés el 15/12/2019",
+		texto: "",
+		firma: "",
+		fecha: "15 de diciembre 2019",
+		pdf: true,
+		documento: 'doc4',
+		ruta: 'grupo-castreños-retomar-proyecto-agora'
+	},
+
+	{
+		id: 3,
+		titulo: "Recogen firmas para que se ejecute el Centro Multisusos Ágora de Castro",
+		subtitulo: "Artículo publicado en la edición de papel de El Diario Montañés el 07/12/2019",
+		texto: "",
+		firma: "",
+		fecha: "7 de diciembre 2019",
+		pdf: true,
+		documento: 'doc3',
+		ruta: 'firmas-centro-multiusos-agora-castro'
+	},
+
+	{
+		id: 2,
+		titulo: "Más ocio en Castro Urdiales",
+		subtitulo: "Carta publicada en El Diario Montañés el 01/12/2019",
+		texto: "Leo con estupor en el periódico la noticia de 'Castro tendrá que pagar 138.000 euros tras renunciar al proyecto de hacer un cine y un teatro'. Las razones del nuevo equipo de gobierno para tumbar el proyecto están bien argumentadas: 'no nos podemos conformar con parches' para el que el consistorio considera uno de los municipios más importantes de Cantabria. De modo que se cancela un proyecto para alcanzar uno más ambicioso. De nuevo nos encontramos con el olvido o rechazo de lo empezado para volver a empezar. Sucede tantas veces que pareciera que es el deporte nacional. Ocurre que este municipio tan importante no tiene ni un lugar con condiciones para ver cine o teatro y lleva décadas con planes. Y cuando parecía que se estaba cerca de conseguir algo en el centro del pueblo, no sólo se cancela el proyecto sino que encima cuesta dinero. Después se nos propone que 'se estudiará la reubicación', en un lugar todavía por aprobar, sin proyecto, solo con deseos y ambiciones, sin presupuestos ni partidas... para mí son castillos en el aire.",
+		firma: "Antonio Zornoza López",
+		fecha: "1 de diciembre 2019",
+		pdf: false,
+		documento: '',
+		ruta: 'mas-ocio-en-castrp-urdiales'
+	},
+
+	{	
+		id: 1,
+		titulo: "El ocio en Castro Urdiales",
+		subtitulo: "Carta publicada en El Diario Montañés el 23/11/2019",
+		texto: "El gobierno municipal de Castro Urdiales rechaza la construcción de una sala multidisciplinar (incluyendo teatro/cine) que resuelve una carencia que se remonta a 1980, argumentando que 'esta ciudad se merece algo mejor'. Teatro y cine no son simples actividades lúdicas. Son cultura y además generan en todo el espectro de edades un ámbito de relación personal y social de la que esta sociedad actual está carente. Siendo una obra urgente y necesaria, que beneficia las relaciones sociales entre la ciudadanía, facilita el acceso a la cultura y revitaliza la actividad en la ciudad; estando únicamente a falta de licitación pública, y que en ningún caso es impedimento para otros proyectos más ambiciosos, no hay razón ninguna para que no se lleve adelante. La clase política de este país está en deuda con la sociedad, y no puede dejar escapar oportunidades como esta para, con amplitud de miras y por encima de consideraciones 'sectarias, infantiles y dogmáticas', desarrollar dotaciones que benefician al conjunto de la sociedad, no debemos perder de vista que la cultura es de todos y está pensada para todas las personas.",
+		firma: "Cinta Marraco",
+		fecha: "23 de noviembre 2019",
+		pdf: false,
+		documento: '',
+		ruta: 'ocio-en-castro-urdiales'
+	}
+];
+
+module.exports = pressPosts;
